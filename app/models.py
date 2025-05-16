ea15b1bdda68ebc8ae759eff6ebfc43e5e047dd4
umar <amar.m.sheikh@gmail.com>
2025-05-16 13:57:31 +0500

    Add validations on task name of Todo Model

--- app/models.py
+++ app/models.py
@@ -1,7 +1,8 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
 class Todo(models.Model):
-    task_name=models.CharField(max_length=50)
+    task_name=models.CharField(max_length=50, unique=True, null=False, validators=[MinLengthValidator(3)])
     task_description=models.TextField(null=True, blank=True)
     is_completed=models.BooleanField(default=False)
 
